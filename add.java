62009f51f36c03a9e8a571c51ee0c63f05417a95
じゅういち <160393316+School-account-10@users.noreply.github.com>
2024-02-18 10:28:24 +0000

    Add files via upload

---
+++ add.java
@@ -0,0 +1,5 @@
+public class add { 
+    public static void main(String[] arg) {
+        System.out.println("1x1=" + 1*1 + " 1x2=" + 1*2 + " 1x3=" + 1*3 + " 1x4=" + 1*4 + " 1x5=" + 1*5 + " 1x6=" + 1*6 + " 1x7=" + 1*7 + " 1x8=" + 1*8 + " 1x9=" + 1*9 + " 1x10=" + 1*10);
+    }
+}
